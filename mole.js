03ef3008222cf8f4c57d3c92737a414f77c5bbae
Tanner Schmutte <tannerschmutte@gmail.com>
2020-11-04 13:18:22 -0800

    whacking the mole

--- mole.js
+++ mole.js
@@ -1,8 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    const moleHeads = document.querySelectorAll(".wgs__mole-head");
-    for (let moleHead of moleHeads) {
-      moleHead.classList.toggle("wgs__mole-head--hidden");
-    }
-  }, 1000);
-});
+  function popUpRandomMole() {
+    let moles = document.querySelectorAll(".wgs__mole-head")
+    let randomNum = Math.floor((Math.random() * 8))
+
+    let currentMole = moles[randomNum]
+    currentMole.classList.remove("wgs__mole-head--hidden")
+
+    setTimeout(hideMole, 3000, currentMole)
+  
+  }
+
+  function hideMole(mole) {
+    mole.classList.add("wgs__mole-head--hidden")
+
+    setTimeout(popUpRandomMole, 1000)
+  }
+
+  setTimeout(popUpRandomMole, 0)
+
+
+  document.querySelector(".pf").addEventListener("click", (e) => {
+    e.target.classList.add("wgs__mole-head--hidden")
+  })
+})
